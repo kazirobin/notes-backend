04f407a91f3fb71c48fde3b5b4878a1fdcf8915c
kazirobin <kazirobinking@gmail.com>
2025-09-06 18:19:55 +0600

    start learning http

--- nodejs/http.js
+++ nodejs/http.js
@@ -2,11 +2,14 @@ const http = require("http");
 const fs = require("fs");
 const myServer = http.createServer((req, res) => {
   console.log(req);
-  if(req.url !== "/favicon.ico"){
-
-    const log = `\n${Date.now()}:New Req Recived`
-    fs.appendFile("./log.txt",log,(err) => { console.log(err) })
+  if (req.url !== "/favicon.ico") {
+    const log = `\n${Date.now()}:New Req Recived`;
+    fs.appendFile("./log.txt", log, (err) => {
+      console.log(err);
+    });
   }
   res.end("Hello from server");
 });
-myServer.listen(8000,() => { console.log("Server Stared..") })
+myServer.listen(8000, () => {
+  console.log("Server Stared..");
+});
